c9c751d780058cd4a840ece73064ae74ea95ddd4
Willian <joiltonwillian@gmail.com>
2026-01-16 03:15:10 -0300

    Atualizando sistema de linguagem
    Apenas ordenei as variáveis e deixei funcional a opção de alterar os elementos, a seta em SHOP NOW ficou meio torta, recomendo olhar depois, ainda falta o header para traduzir

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 const translations = {
+    
     English: {
+
         Selct : "Select Language", 
-        title:" Winter Collection",
-        pargr: "New Winter",
-        pargr2: "Collection 2026",
-        pargr3: "There's Nothing like Tren",
-        pargr4: "Shop Now"
+        title: "Winter Collection",
+        pargr: "New Winter \n Collection 2026",
+        pargr2: "There's Nothing like Trend!",
+        pargr3: "Shop Now"
     },
 
     Portuguese: {
+
         Selct : "Selecione o Idioma",
         title : "Coleção de Inverno",
-        pargr: "Novo Inverno",
-        pargr2: "Coleção 2026",
-        pargr3: "Não Há Nada Como Trend",
-        pargr4: "Compre Agora"
+        pargr: "Novo Inverno \n Coleção 2026",
+        pargr2: "Não Há Nada Como Uma Tendência!",
+        pargr3: "Compre Agora"
     }
 }
+
 
 const LanguageSelectop = document.querySelector('Select');
 
 let h5 = document.querySelector('h5');
 let h1= document.querySelector('h1');
-let par = document.querySelector('par');
-let pargr2 = document.querySelector('pargr2');
-let pargr3 = document.querySelector('pargr3');
-let pargr4 = document.querySelector('pargr4');
-
-
+let par = document.querySelector('p.parq');
+let Shop = document.querySelector('div.teste');
 
 LanguageSelectop.addEventListener("change", (event) => {
     setLanguage(event.target.value)
@@ -35,23 +33,22 @@ LanguageSelectop.addEventListener("change", (event) => {
 })
 
 const setLanguage = (Language) => {
-    if(Languag=="English"){
-        h5.innerText = translations.English.Selct;
-        title.innerText = translations.English.Selct;
-        par.innerText = translations.English.Selct;
-        pargr2.innerText = translations.English.Selct;
-        pargr3.innerText = translations.English.Selct;
-        pargr4.innerText = translations.English.Selct;
+    if(Language=="English"){
 
-        console.log(Language);
+        h5.innerText = translations.English.title;
+        h1.innerText = translations.English.pargr;
+        par.innerText = translations.English.pargr2;
+        Shop.innerText = translations.English.pargr3;
+        return;
+
 } else  if (Language=="Portuguese")
-       h5.innerText = translations.Portuguese.Selct
-        title.innerText = translations.Portuguese.Selct
-        par.innerText = translations.Portuguese.Selct
-        pargr2.innerText = translations.Portuguese.Selct
-        pargr3.innerText = translations.Portuguese.Selct
-        pargr4.innerText = translations.Portuguese.Selct
-        console.log(Language);
+        h5.innerText = translations.Portuguese.title;
+        h1.innerText = translations.Portuguese.pargr;
+        par.innerText = translations.Portuguese.pargr2;
+        Shop.innerText = translations.Portuguese.pargr3;
+
+        
 }
 
-    
+
+
