aa9545e7d62ac494086bcef707dfce6a8b401a8a
Anaíse Gomes <110070867+anaisegomes@users.noreply.github.com>
2026-01-17 20:45:01 -0300

    new
    atualizações

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const translations = {
         title: "Winter Collection",
         pargr: "New Winter \n Collection 2026",
         pargr2: "There's Nothing like Trend!",
-        pargr3: "Shop Now"
+        pargr3: "Shop Now",
+        pargr4: "Our Trending Products",
+        pargr5: "Sale"
     },
 
     Portuguese: {
@@ -15,7 +17,9 @@ const translations = {
         title : "Coleção de Inverno",
         pargr: "Novo Inverno \n Coleção 2026",
         pargr2: "Não Há Nada Como Uma Tendência!",
-        pargr3: "Compre Agora"
+        pargr3: "Compre Agora",
+        pargr4: "Nossos Produtos em Tendências",
+        pargr5: "Venda"
     }
 }
 
@@ -26,6 +30,8 @@ let h5 = document.querySelector('h5');
 let h1= document.querySelector('h1');
 let par = document.querySelector('p.parq');
 let Shop = document.querySelector('div.teste');
+let pargr4 = document.querySelector('div.center-text');
+let pargr5 = document.querySelector('div.product-'); 
 
 LanguageSelectop.addEventListener("change", (event) => {
     setLanguage(event.target.value)
@@ -39,6 +45,8 @@ const setLanguage = (Language) => {
         h1.innerText = translations.English.pargr;
         par.innerText = translations.English.pargr2;
         Shop.innerText = translations.English.pargr3;
+        pargr4.innerText = translations.English.pargr4;
+        pargr5.innerText = translations.English.pargr5
         return;
 
 } else  if (Language=="Portuguese")
@@ -46,7 +54,8 @@ const setLanguage = (Language) => {
         h1.innerText = translations.Portuguese.pargr;
         par.innerText = translations.Portuguese.pargr2;
         Shop.innerText = translations.Portuguese.pargr3;
-
+        pargr4.innerText = translations.Portuguese.pargr4,
+        pargr5.innerText = translations.Portuguese.pargr5
         
 }
 
