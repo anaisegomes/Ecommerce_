a1617e87428c0ef0b7cecad6fa79bab0ad19ced3
Anaíse Gomes <110070867+anaisegomes@users.noreply.github.com>
2026-01-18 18:50:44 -0300

    true

--- AboutUs.js
+++ AboutUs.js
@@ -4,16 +4,19 @@ const translations = {
 
         Selct : "Select Language", 
         h1: "About Us (Kumon)",
-        h4: "How did Kumon start?",
-        pargr2: "Kumon was born from the creativity of a fashion student living in Brazil. /n In 2021, her dream moved forward with the help of her mother /n Since then, the store has been selling clothes for women to feel good about themselves."
+        h1: "How did Kumon start?",
+        h4: "Kumon was born from the creativity of a fashion student living in Brazil. /n In 2021, her dream moved forward with the help of her mother /n Since then, the store has been selling clothes for women to feel good about themselves.",
+        par: "Kate loved designing clothes since she was little and saw an opportunity for her creativity to come to life.",
+        par2: "In 2021, her dream moved forward with the help of her mother.",
+        par3: "Since then, the store has been selling clothes for women to feel good about themselves.",
     },
 
     Portuguese: {
 
         Selct : "Selecione o idioma",
         h1 : "Sobre Nós (Kumon)",
-        h4: "Como a Kumon começou?",
-        pargr2: "A Kumon surgiu da criatividade de uma estudante de moda que mora no brasil, /n Kate amava desenhar roupas desde pequenas e viu uma oportunidade da sua criatividade sair do papel /n Desde então a loja vende roupa para mulheres se sentirem bem consigo mesmas"
+        h1: "Como a Kumon começou?",
+        h4: "A Kumon surgiu da criatividade de uma estudante de moda que mora no brasil, /n Kate amava desenhar roupas desde pequenas e viu uma oportunidade da sua criatividade sair do papel /n Desde então a loja vende roupa para mulheres se sentirem bem consigo mesmas"
     }
 }
 
@@ -21,7 +24,7 @@ const translations = {
 const LanguageSelectop = document.querySelector('Select');
 
 let h1 = document.querySelector('h1');
-let h4= document.querySelector('h4');
+let h1= document.querySelector('h1.h1');
 let pargr2 = document.querySelector('p.parqr2');
 
 LanguageSelectop.addEventListener("change", (event) => {
